0ec8a3e45a4fae1044379e86426beb842c72e388
Noam Korobko <noam.korobko@volumez.com>
2022-04-13 17:09:00 +0300

    Adding random suffix to the pg name to differentiate from others

--- terraform/aws/modules/placement_group/main.tf
+++ terraform/aws/modules/placement_group/main.tf
@@ -2,9 +2,14 @@ terraform {
     required_version = ">=0.14"
 }
 
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
+}
+
 resource "aws_placement_group" "env_pg" {
     count    = var.num_of_zones
     
-    name     = "Volumez-pg-${count.index}"
+    name     = "Volumez-pg-${count.index}-${random_string.suffix.result}"
     strategy = var.placement_group_strategy
 }
