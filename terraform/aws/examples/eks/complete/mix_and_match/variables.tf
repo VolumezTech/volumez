3cc41cf519ed66ba427e991cdab3a9754b74da2e
Itay Ginor <itay.ginor@volumez.com>
2023-02-21 12:39:51 +0200

    Updated tf variables with k8s version and nodes ami type

--- terraform/aws/examples/eks/complete/mix_and_match/variables.tf
+++ terraform/aws/examples/eks/complete/mix_and_match/variables.tf
@@ -3,6 +3,12 @@ variable "region" {
   description = "Please enter the region. For example: us-east-1"
 }
 
+variable "k8s_version" {
+  type        = string
+  description = "Kubernetes version"
+  default     = "1.21"
+}
+
 variable "cluster_owner" {
   type        = string
   description = "Please enter the cluster owner name/tag"
@@ -23,7 +29,27 @@ variable "media_node_type" {
   description = "Please enter EC2 Instance Type for media nodes. For example: 'i3.large'. You can see a list of them here: https://aws.amazon.com/ec2/instance-types/ "
 }
 
+variable "media_node_ami_type" {
+  type        = string
+  description = "media node ami type - AL2_x86_64/AL2_ARM_64"
+  default     = "AL2_x86_64"
+  validation {
+    condition     = contains(["AL2_x86_64", "AL2_ARM_64"], var.media_node_ami_type)
+    error_message = "Valid values for var: media_node_ami_type are (AL2_x86_64, AL2_ARM_64)."
+  } 
+}
+
 variable "app_node_type" {
   type        = string
   description = "Please ente EC2 Instance Type for application nodes. For example: 'i3.large'. You can see a list of them here: https://aws.amazon.com/ec2/instance-types/ "
+}
+
+variable "app_node_ami_type" {
+  type        = string
+  description = "app node ami type - AL2_x86_64/AL2_ARM_64"
+  default     = "AL2_x86_64"
+  validation {
+    condition     = contains(["AL2_x86_64", "AL2_ARM_64"], var.app_node_ami_type)
+    error_message = "Valid values for var: app_node_ami_type are (AL2_x86_64, AL2_ARM_64)."
+  } 
 }
