fad5e8975662d56874afa07fbf28a5dc7bd8d7f0
Noam Korobko <noam.korobko@volumez.com>
2024-03-27 22:40:58 +0200

    NK: fixing the dns names

--- terraform/oci/instance-pool/security-rules.tf
+++ terraform/oci/instance-pool/security-rules.tf
@@ -1,7 +1,7 @@
 resource "oci_core_security_list" "volumez-sl" {
   compartment_id = var.compartment_ocid
   vcn_id         = oci_core_vcn.test_vcn.id
-  display_name   = "volumez-sl"
+  display_name   = "volumez-sl-${random_string.deploy_id.result}"
 
   ingress_security_rules {
     protocol  = "all"
