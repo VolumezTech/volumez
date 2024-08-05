a901ac06e62f63b3a8e32e58f4c34984d0c1816a
Itay Ginor <itay.ginor@volumez.com>
2024-08-05 16:37:02 +0300

    revmoce default value from copy image

--- terraform/oci/copy-image/var.tf
+++ terraform/oci/copy-image/var.tf
@@ -1,7 +1,7 @@
-variable compartment_id { default = "ocid1.tenancy.oc1..aaaaaaaaij7gdgmcldaneftxyrhxiwpnaecvenjb42423cxhxmewlf65ca2q" }
-variable region { default = "us-phoenix-1" }
-variable custom_image_ocid {default = "ocid1.image.oc1.phx.aaaaaaaatseay3zx33ai5rwww6hwgjg2p62y7nlwmlpkj4amsb3shukmv4iq"}
-variable bucket_name { default = "custom-image-bucket"}
-variable os_namespace { default = "idhacflgsnvm"}
-variable imported_image_name { default = "BM-5.15-ubuntu22.04-cgroupsv1"}
-variable base_image_name { default = "BM-5.15-ubuntu22.04-cgroupsv1"}
+variable compartment_id {}
+variable region {}
+variable custom_image_ocid {}
+variable bucket_name {}
+variable os_namespace {}
+variable imported_image_name {}
+variable base_image_name {}
